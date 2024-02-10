d5843dbc3408d2fac89f84bd711d2887ff9dfa3e
Esra <esraklc081@gmail.com>
2024-02-10 21:51:44 +0300

    Mask Distribution
    C# Basics

---
+++ MaskTracking/ClassLibrary1/Concrete/PersonManager.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Concrete;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager
+    {
+        // encapsulation
+        public void ApplyForMask(Person person)
+        {
+
+        }
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            if (person.ID != 0)
+            {
+                // control conditions from system data
+                KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+                return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(person.ID, person.FirstName, person.LastName, person.DateOfBirthyear))).Result.Body.TCKimlikNoDogrulaResult;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+            
+    }
+}
---
+++ MaskTracking/Workspace/Program.cs
@@ -0,0 +1,102 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables();
+
+            Vatandas vatandas1 = new Vatandas();
+
+            int result1 = Sum(3,5);
+
+            Person person1 = new Person();
+            person1.FirstName = "Esra";
+            person1.LastName = "KILIÇ";
+            person1.DateOfBirthyear = 2003;
+            person1.ID = 123;
+
+            //Arrays
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Elvan";
+            ogrenciler[1] = "Ezgi";
+            ogrenciler[2] = "Ceren";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "Emira";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] {"Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır"};
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            Person person1 = new Person();
+            person1.FirstName = "Eren";
+
+            foreach(string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            List<string> yenisehirler1 = new List<string> { "Ankara 1", "İstanbul 1", "İzmir 1" };
+            yenisehirler1.Add("Adana 1");
+            foreach(var sehir in yenisehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            PttManager pttManager = new PttManager(new ForeignManager());
+            pttManager.GiveMask(person1);
+        }
+
+        
+        static void SayHello(string name = "isimsiz")
+        {
+            Console.WriteLine("Hello" + name);
+        }
+
+        private static void Variables()
+        {
+            string message = "Merhaba";
+            double amount = 10000; //db den gelir
+            int number = 100;
+            bool isLogin = false;
+
+            string name = "Esra";
+            string surname = "Kılıç";
+            int birthyear = 2003;
+            long ID = 12345678910;
+
+            Console.WriteLine(amount * 1.18);
+            Console.ReadLine();
+        }
+
+        public class Vatandas
+        {
+            public string name { get; set; }
+            public string surname { get; set; }
+            public int birthyear { get; set; }
+            public long ID { get; set; }
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine( "Addition:" + result );
+            return result;
+        }
+
+    }
+
+}
